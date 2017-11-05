7ab3ec2c822dc0f0832cd62252539bad6c826c20
Alexandre <alexandrebing@gmail.com>
2017-11-05 12:17:34 -0200

    Reverti mudança anterior no teste de autor

--- src/Jogo/Main.java
+++ src/Jogo/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        //VER SE HOUVE MUDANÇA
         RunGame game = new Jogo.RunGame();
         System.out.println("1 - Novo Jogo | 2 - Carregar Jogo");
         Scanner in = new Scanner(System.in);
